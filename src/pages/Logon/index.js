c76ee311c74820bd9a73075e6eb674b58d08c99d
Wilson Neto <wilsonpsn@hotmail.com>
2020-03-25 21:12:26 -0300

    INtegrado o Login

--- src/pages/Logon/index.js
+++ src/pages/Logon/index.js
@@ -3,19 +3,37 @@ import { FiLogIn } from 'react-icons/fi';
 
 import api from '../../services/api';
 
-import { Link } from 'react-router-dom';
+import { Link, useHistory } from 'react-router-dom';
 import './styles.css';
 
 import logoImg from '../../assets/logo.svg';
 import heroesImg from '../../assets/heroes.png';
-const index = () => {
+const Logon = () => {
+  const [id, setId] = useState('');
+  const history = useHistory();
+  const handleLogin = async evt => {
+    evt.preventDefault();
+    try {
+      const response = await api.post('sessions', { id });
+      localStorage.setItem('ongId', id);
+      localStorage.setItem('ongName', response.data.name);
+      history.push('/profile');
+    } catch (err) {
+      alert('Falha no login tente novamente');
+    }
+  };
   return (
     <div className="logon-container">
       <section className="form">
         <img src={logoImg} alt="Be The Hero" />
-        <form>
+        <form onSubmit={handleLogin}>
           <h1>Faça seu logon</h1>
-          <input type="text" placeholder="id" />
+          <input
+            type="text"
+            placeholder="id"
+            value={id}
+            onChange={e => setId(e.target.value)}
+          />
           <button className="button" type="submit">
             Entrar
           </button>
@@ -30,4 +48,4 @@ const index = () => {
   );
 };
 
-export default index;
+export default Logon;
